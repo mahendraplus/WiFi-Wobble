0d5881a46ed0992515ae3673926f9ebd9176269e
ᨀ Max ᨀ <57547869+mahendraplus@users.noreply.github.com>
2022-11-05 20:56:15 +0545

    Update wifi-wobble.sh

--- wifi-wobble.sh
+++ wifi-wobble.sh
@@ -1,1 +1,10 @@
-pending....
+#!/bin/bash
+sudo apt update
+sudo apt upgrade
+sudo apt install wget
+sudo apt install curl
+sudo apt install esptool
+mkdir WiFi-Wobble
+cd WiFi-Wobble
+wget https://github.com/mahendraplus/WiFi-Wobble/releases/download/Wifi%EA%9A%B8Wobble/wobble-nodemcu-esp8266.bin
+esptool write_flash -fs 4MB -ff 80m -fm dio 0x0 wobble-nodemcu-esp8266.bin
